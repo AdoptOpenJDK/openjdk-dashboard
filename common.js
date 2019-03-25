c796d97a68efaf2d7c0cebf299acf6b04bbd629d
George Adams <georgeadams1995@gmail.com>
2019-03-25 17:58:56 +0000

    refactor graph function

--- common.js
+++ common.js
@@ -1,3 +1,63 @@
+function generateGraph(labels, downloads, description) {
+  var graphVersion = document.getElementById('graph').getContext('2d');
+  Chart.defaults.global.defaultFontSize = 20;
+  var myChart = new Chart(graphVersion, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: description,
+        data: downloads,
+        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      "hover": {
+        "animationDuration": 0
+      },
+      "animation": {
+        "duration": 1,
+        "onComplete": function() {
+          var chartInstance = this.chart,
+            ctx = chartInstance.ctx;
+
+          ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+          ctx.textAlign = 'center';
+          ctx.textBaseline = 'bottom';
+          this.data.datasets.forEach(function(dataset, i) {
+            var meta = chartInstance.controller.getDatasetMeta(i);
+            meta.data.forEach(function(bar, index) {
+              var data = dataset.data[index];
+              ctx.fillText(addComma(data), bar._model.x, bar._model.y - 5);
+            });
+          });
+        }
+      },
+      tooltips: {
+        "enabled": false
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }],
+        xAxes: [{
+          stacked: false,
+          beginAtZero: true,
+          ticks: {
+            stepSize: 1,
+            min: 0,
+            autoSkip: false
+          }
+        }],
+      }
+    }
+  });
+}
+
 function addComma(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
--- version.js
+++ version.js
@@ -21,66 +21,7 @@ function graph(version, tag) {
               downloads.push(platform.installer_download_count)
             }
           }
-          var ctx = document.getElementById("graph");
-          Chart.defaults.global.defaultFontSize = 20;
-          var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-              labels: labels,
-              datasets: [{
-                label: 'Summary of ' + tag + ' Downloads',
-                data: downloads,
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
-              }]
-            },
-            options: {
-              "hover": {
-                "animationDuration": 0
-              },
-              "animation": {
-                "duration": 1,
-                "onComplete": function() {
-                  var chartInstance = this.chart,
-                    ctx = chartInstance.ctx;
-
-                  ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
-                  ctx.textAlign = 'center';
-                  ctx.textBaseline = 'bottom';
-                  this.data.datasets.forEach(function(dataset, i) {
-                    var meta = chartInstance.controller.getDatasetMeta(i);
-                    meta.data.forEach(function(bar, index) {
-                      var data = dataset.data[index];
-                      ctx.fillText(addComma(data), bar._model.x, bar._model.y - 5);
-                    });
-                  });
-                }
-              },
-              tooltips: {
-                "enabled": false
-              },
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }],
-                xAxes: [{
-                  stacked: false,
-                  beginAtZero: true,
-                  ticks: {
-                    maxRotation: 90,
-                    minRotation: 90,
-                    stepSize: 1,
-                    min: 0,
-                    autoSkip: false
-                  }
-                }],
-              }
-            }
-          });
-
+          generateGraph(labels, downloads, `Summary of ${tag} Downloads`)
         });
       }
     )
